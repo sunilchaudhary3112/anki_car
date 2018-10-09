197f6c68b1183f9303b6d4de8d06bf1de6a4462a
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2018-10-09 17:19:42 +0530

    Anki car impact event usecase

--- anki_car.js
+++ anki_car.js
@@ -1,84 +1,58 @@
 // //import {Bluetooth, VehicleScanner} from "anki-overdrive-api"
 var anki = require('anki-overdrive-api');
-
 console.log(anki);
 const bluetooth = new anki.Bluetooth()
 const scanner = new anki.VehicleScanner(bluetooth)
-
 // scanner.findAll().then(vehicles => {
 //     // Do something with vehicles...
 //     console.log(vehicles);
 // }).catch(/* Handle Errors */)
 
+var blue_car = "eaa44ead7392";
+var red_car = "c773068b8c30";
+var red_black = "c55edb917115";
 
-scanner.findById("eaa44ead7392").then(vehicle => {
+scanner.findById(red_car).then(vehicle => {
     console.log(vehicle);
     // Do something with vehicle...
-
     vehicle.addListener(function (message) {
         console.log(message);
         // console.log("drove over location: " + message.locationId);
         // console.log("current speed: " + message.speedMmPerSec);
         // Do something else with the message...
     });
-
     vehicle.connect().then(() => {
-
-        vehicle.setSpeed(100);
-
+        vehicle.setSpeed(200);
         // Vehicles are using offset for positioning.
         //vehicle.changeLane(0);
         var i = 0;
-        while (i < 1000) {
-
+        while (i < 1500) {
+           //vehicle.setSpeed(400);
             if (i < 200) {
-                vehicle.setSpeed(250);
+                vehicle.setSpeed(200);
             }
             else if (i > 200 & i < 250) {
-                vehicle.setSpeed(0);
+               vehicle.setSpeed(0);
             }
-            else if (i > 250 & i < 350) {
+            else if (i > 250 & i < 450) {
+                vehicle.setSpeed(600);
+            } else if (i > 450 & i < 500) {
+                vehicle.setSpeed(0);
+            }else if (i > 500 & i < 505) {
+                vehicle.setSpeed(400);
+                //vehicle.changeLane(-23);
+                vehicle.uTurn(true);
+            }
+			else if(i > 505 && i < 600 ) {
+				vehicle.setSpeed(400);
+			}else if (i > 600 & i < 605) {
                 vehicle.setSpeed(500);
-            } else if (i > 310 & i < 500) {
-                vehicle.setSpeed(0);
-            }
-
+                //vehicle.changeLane(-23);
+                //vehicle.uTurn(true);
+            }else if(i > 605 && i < 1000 ) {
+				vehicle.setSpeed(500);
+			}
             i++;
         }
-
     });
 });
-
-
-// const anki = require('anki-overdrive-sdk');
-// const eventemitter = anki.eventemitter;
-// eventemitter.on('carReady', (device) => {
-//     // New device
-//     console.log('Device is ready to use');
-//     console.log(device);
-//     anki.setSpeed(device.id, 700, 5);
-
-// });
-
-// eventemitter.on('carStatusMessage', (device, status) => {
-//     // New car status
-//     console.log(status);
-// })
-
-// anki.setUp()
-//     .then(() => {
-//         // The connection is established and you can start to send commands now
-//     })
-//     .catch((err) => {
-//         throw new Error(err)
-//     });
-
-
-// function exitHandler(options, err) {
-//     anki.disconnectAll()
-//     process.exit()
-// }
-
-// process.on('exit', exitHandler.bind(null, { cleanup: true }))
-// process.on('SIGINT', exitHandler.bind(null, { exit: true }))
-// process.on('uncaughtException', exitHandler.bind(null, { exit: true }))
